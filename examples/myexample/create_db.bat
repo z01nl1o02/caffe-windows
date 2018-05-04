d236b76ece3a04c4292443682953a950fcc1bebf
ss <ss@01>
2018-05-04 17:06:30 +0800

    add format about train.txt

--- examples/myexample/create_db.bat
+++ examples/myexample/create_db.bat
@@ -1,8 +1,10 @@
 @setlocal enabledelayedexpansion
+::train.txt and test.txt should be in format as following
+::classid\pic.jpg
+::classid should be int
+set cafferoot=C:\dev\online\github\dev-cs\caffe-windows\
 
-set cafferoot=c:\dev\online\github\dev-happynear\caffe-windows
-
-!cafferoot!\Build\x64\Release\convert_imageset.exe --shuffle --resize_width=256 --resize_height=256 !cafferoot!\data\re\ !cafferoot!\examples\myfile\train.txt !cafferoot!\examples\myfile\img_train_lmdb
-!cafferoot!\Build\x64\Release\convert_imageset.exe --shuffle --resize_width=256 --resize_height=256 !cafferoot!\data\re\ !cafferoot!\examples\myfile\test.txt !cafferoot!\examples\myfile\img_test_lmdb
+!cafferoot!\Build\x64\Release\convert_imageset.exe --shuffle --resize_width=32 --resize_height=32 C:\dataset\cifar\split\train\ !cafferoot!\examples\myexample\train.txt !cafferoot!\examples\myexample\img_train_lmdb
+!cafferoot!\Build\x64\Release\convert_imageset.exe --shuffle --resize_width=32 --resize_height=32 C:\dataset\cifar\split\test\ !cafferoot!\examples\myexample\test.txt !cafferoot!\examples\myexample\img_test_lmdb
 
 @endlocal
